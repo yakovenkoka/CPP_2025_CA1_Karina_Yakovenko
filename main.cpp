115b00e7e42499e974a8cdec45145c4000e63f04
yakovenkoka <D00272517@student.dkit.ie>
2025-02-28 00:24:54 +0000

    Done stage 4

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <limits.h>
 #include <list>
+#include <algorithm>
 
 using namespace std;
 
@@ -24,7 +25,7 @@ void displayPhone(const Phone& p) {
     << setw(35) << p.model
     << setw(15) << p.releaseYear
     << setw(15) << fixed << setprecision(2) << p.price
-    << setw(10) << fixed << setprecision(1) << p.screenSize
+    << setw(10) << fixed << setprecision(2) << p.screenSize
     << endl;
 }
 
@@ -72,7 +73,7 @@ void displayAllPhones(const vector<Phone>& phones) {
     }
 }
 
-// Search for a phone by model
+// Search index of phone by model
 int searchPhoneByModel(const vector<Phone>& phones, const string& model) {
     for (int i = 0; i < phones.size(); i++) {
         if (phones[i].model == model) {
@@ -94,15 +95,6 @@ map<string, int> countPhonesByBrand(const vector<Phone>& phones) {
 // Function to display phones of a particular brand
 void displayPhonesByBrand(const vector<Phone>& phones, const string& brand) {
     bool found = false;
-    cout << "Phones of brand: " << brand << endl;
-    cout << left
-    << setw(15) << "Brand"
-    << setw(35) << "Model"
-    << setw(15) << "Release Year"
-    << setw(15) << "Price"
-    << setw(10) << "Screen Size"
-    << endl;
-
     for (const Phone& p : phones) {
         if (p.brand == brand) {
             displayPhone(p);
@@ -111,7 +103,8 @@ void displayPhonesByBrand(const vector<Phone>& phones, const string& brand) {
     }
     if (!found) {
         cout << "No phones found for brand: " << brand << endl;
-    }
+    } 
+
 }
 
 //Function to find the highest, lowest and average of a discrete numerical field (Integer)
@@ -167,58 +160,111 @@ void displayPhonesInDescendingOrder(const vector<Phone>& phones){
     }
 }
 
+// Function to display the menu
+void displayMenu() {
+    cout << "\n----Menu----" << endl;
+    cout << "\n1. Display all phones" << endl;
+    cout << "2. Search index of phone by model" << endl;
+    cout << "3. Count the number of phones of each brand" << endl;
+    cout << "4. Display phones of a particular brand" << endl;
+    cout << "5. Find Highest, Lowest, and Average Release Year" << endl;
+    cout << "6. Search Phones by Partial Text\n";
+    cout << "7. Display Phones in Descending Order of Price\n";
+    cout << "8. Exit" << endl;
+}
+
 int main() {
     vector<Phone> phones;
     loadPhones("MOCK_DATA.csv", phones);
-    displayAllPhones(phones);
 
-    // Search for a phone by model
-    string model;
-    cout << "\nEnter model to search: ";
-    getline(cin, model);
-    int index = searchPhoneByModel(phones, model);
-    if (index != -1) {
-        cout << "Phone found for index: " << index << endl;
-        displayPhone(phones[index]);
+    bool exit = false;
+    while (!exit) {
+        displayMenu();
+
+        string input;
+        int choice;
+        cout << "\nEnter choice: ";
+        getline(cin, input);
+        
+        try{
+            choice = stoi(input);
+        } catch (exception e) {
+            cout << "Invalid choice" << endl;
+            continue;
+        }
+
+        switch (choice) {
+            // Display all phones
+            case 1:
+                displayAllPhones(phones);
+                break;
+            case 2: {
+                // Search index of phone by model
+                string model;
+                cout << "\nEnter model to search: ";
+                getline(cin, model);
+                int index = searchPhoneByModel(phones, model);
+                if (index != -1) {
+                    cout << "Phone found for index: " << index << endl;
+                    displayPhone(phones[index]);
+                }
+                else {
+                    cout << "Phone not found" << endl;
+                }
+                break;
+            }
+            case 3: {
+                // Count the number of phones of each brand
+                map<string, int> count = countPhonesByBrand(phones);
+                cout << "\n----Count of phones by brand----" << endl;
+                for (const auto& brandCount : count) {
+                    cout << brandCount.first << ": " << brandCount.second << endl;
+                }
+                break;
+            }
+            case 4: {
+                // Display phones of a particular brand
+                string filterBrand;
+                cout << "\nEnter brand to filter: ";
+                getline(cin, filterBrand);
+                displayPhonesByBrand(phones, filterBrand);
+                break;
+            }
+            case 5: {
+                // Find Highest, Lowest, and Average Release Year
+                Phone max, min;
+                int avgReleaseYear = findMaxMinAvgReleaseYear(phones, max, min);
+                cout << "\nAverage release year: " << avgReleaseYear << endl;
+                cout << "Phone with highest release year: \t";
+                displayPhone(max);
+                cout << "Phone with lowest release year: \t";
+                displayPhone(min);
+                break;
+            }
+            case 6: {
+                // Search Phones by Partial Text
+                string text;
+                cout << "\nEnter text to search in model: ";
+                getline(cin, text);
+                list<Phone> matchingPhones = searchPhoneByPartialText(phones, text);
+                
+                for (const Phone& p : matchingPhones) {
+                    displayPhone(p);
+                }
+                break;
+            }
+            case 7: {
+                // Display Phones in Descending Order of Price
+                displayPhonesInDescendingOrder(phones);
+                break;
+            }
+            case 8:
+                exit = true;
+                break;
+            default:
+                cout << "Invalid choice" << endl;
+        }
     }
-    else {
-        cout << "Phone not found" << endl;
-    }
-
-    // Count the number of phones of each brand
-    map<string, int> count = countPhonesByBrand(phones);
-    cout << "\n----Count of phones by brand----" << endl;
-    for (const auto& brandCount : count) {
-        cout << brandCount.first << ": " << brandCount.second << endl;
-    }
-
-    // Display phones of a particular brand
-    string filterBrand;
-    cout << "\nEnter brand to filter: ";
-    getline(cin, filterBrand);
-    displayPhonesByBrand(phones, filterBrand);
-
-    // Find the highest, lowest and average of a discrete numerical field (Integer)
-    Phone max, min;
-    int avgReleaseYear = findMaxMinAvgReleaseYear(phones, max, min);
-    cout << "\nAverage release year: " << avgReleaseYear << endl;
-    cout << "Phone with highest release year: \t";
-    displayPhone(max);
-    cout << "Phone with lowest release year: \t";
-    displayPhone(min);
-
-    // Search for phones that match or partially match a given text input
-    string text;
-    cout << "\nEnter text to search in model: ";
-    getline(cin, text);
-    list<Phone> matchingPhones = searchPhoneByPartialText(phones, text);
-    cout << "\n----Phones with matching text in model----" << endl;
-    for (const Phone& p : matchingPhones) {
-        displayPhone(p);
-    }
-
-    //Display phones in descending order of price
-    displayPhonesInDescendingOrder(phones);
 
     return 0;
 }
